57cc1747e124a3544ee8e68376f287703abc214b
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2025-06-29 01:12:00 +0900

    import_infor_from_api.py

--- baekjoon/import_info_from_api.py
+++ baekjoon/import_info_from_api.py
@@ -1,11 +1,45 @@
 import requests
 
-def get_problem_info(handle):
-    a = requests.get("https://solved.ac/api/v3/user/problem_stats", params={"handle" : "atopi0827"}).json()
-    print(a)
-
+def get_problem_info(nickname):
     try: 
-        a = requests.get("url~", params={"handle":"atopi0827"}).json()
+        a = requests.get("https://solved.ac/api/v3/user/problem_stats", params={"handle" : nickname}).json()
         return a
     except:
-        return "통신 에러..~"
+        return "통신 에러..."
+    
+def top_100_problem_rating():
+    try: 
+        a = requests.get("https://solved.ac/api/v3/user/top_100", params={"handle" : "atopi0827"}).json()
+        print(a)
+        return a
+    except:
+        return "통신 에러..."
+    
+def user_handle_info():
+    try: 
+        a = requests.get("https://solved.ac/api/v3/user/show", params={"handle" : "atopi0827"}).json()
+        print(a)
+        return a
+    except:
+        return "통신 에러..."
+
+
+#problem
+#parmas 부분 parameter로 받아서 처리?
+def problem_to_num():
+    try: 
+        a = requests.get("https://solved.ac/api/v3/problem/lookup", params={"handle" : "atopi0827"}).json()
+        print(a)
+        return a
+    except:
+        return "통신 에러..."
+
+def num_to_problem(problem_num):
+    try: 
+        a = requests.get("https://solved.ac/api/v3/problem/lookup", params={"problemIds" : problem_num}).json()
+        print(a)
+        return a
+    except:
+        return "통신 에러..."
+
+num_to_problem(1000)
