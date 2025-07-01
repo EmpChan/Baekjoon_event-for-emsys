94be81ffd7df03abec80c4ffa22dcdf0da3a866f
Jaechan Hwang <emperor0827@gmail.com>
2025-07-01 21:33:21 +0900

    Add gitignore and change evnet page
    Signed-off-by: Jaechan Hwang <emperor0827@gmail.com>

--- baekjoon/views.py
+++ baekjoon/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        handle = request.POST.handle
+        
     data = {
-        "total_participants" : 3,
+        'total_participants' : '',
     }
     return render(request,'baekjoon/event.html', data)
