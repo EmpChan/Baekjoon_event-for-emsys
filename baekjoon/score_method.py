573e3b2327b4c431428ae5039ba37b595e71c731
Jaechan Hwang <emperor0827@gmail.com>
2025-07-09 19:54:35 +0900

    update score_method
    Signed-off-by: Jaechan Hwang <emperor0827@gmail.com>

--- baekjoon/score_method.py
+++ baekjoon/score_method.py
@@ -3,23 +3,22 @@ from . import import_info_from_api as api
 
 def basicScore(tier,problem_tier,count,isfirst=True):
     score=0
-    problem_base_weight = [1,1,1.1,1.1]
-    problem_additional_weight_upper_zero = [1.35,1.1,1,0.95]
-    problem_additional_weight_under_zero = [0,0.9,1,1.05]
+    problem_base_weight = 1.5
+    problem_lose = [0,0,3,8]
     minimum_score = [10,10,5,2]
     level = max(0,min((tier-3)//5,3))
-    problem_score=problem_tier-tier**(problem_base_weight[level])
+    problem_score=(problem_tier-problem_lose[level]) 
     if problem_score < 0:
-        problem_score*=problem_additional_weight_under_zero[level]
+        problem_score = -(abs(problem_score)**1.5)
     else:
-        problem_score*=problem_additional_weight_upper_zero[level]
+        problem_score **= 1.5
     score=max(minimum_score[level],problem_score+15) 
     w = 0
     if isfirst and count > 0:
        w=50 
     return score*count + w
 
-#API 호출해서 점g수 계산 함수
+#API 호출해서 점수 계산 함수
 def apiScore(obj):
     part = EventParticipants.objects.filter(handle=obj.handle)
     info = api.get_problem_info(obj.handle)
@@ -42,12 +41,12 @@ def makeScore(pid,tiers):
             tier = i['level'],
             tier_solved_cnt= i['solved']
         ).save()
+    print(f'{pid} - well done')
 
 def updateScore():
     partcis = EventParticipants.objects.all()
     for i in partcis:
-        print(i)
         score = Solved.objects.filter(pid=i)
+        print(f'{i} got a additional score : {score}')
         i.score+=apiScore(i)
         i.save()
-        print(i)
